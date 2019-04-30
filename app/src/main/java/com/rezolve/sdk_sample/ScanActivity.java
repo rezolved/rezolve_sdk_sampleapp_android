a972b3c77e0c2fab4a927eae4087794b5f4fa1fc
Artur Teodorowicz <artur.teodorowicz@infullmobile.com>
2019-04-30 10:32:07 +0200

    Added audio scanner

--- app/src/main/java/com/rezolve/sdk_sample/ScanActivity.java
+++ app/src/main/java/com/rezolve/sdk_sample/ScanActivity.java
@@ -16,6 +16,7 @@ import com.rezolve.sdk.model.foreign.RezolveScanResult;
 import com.rezolve.sdk.model.network.RezolveError;
 import com.rezolve.sdk.model.shop.Category;
 import com.rezolve.sdk.model.shop.Product;
+import com.rezolve.sdk.views.RezolveAudioVizualizationView;
 import com.rezolve.sdk.views.RezolveScanView;
 import com.rezolve.sdk_sample.model.ProductDetails;
 
@@ -25,6 +26,7 @@ import org.parceler.Parcels;
 public class ScanActivity extends AppCompatActivity implements ScanManagerInterface {
 
     private RezolveSDK rezolveSdk;
+    private RezolveAudioVizualizationView rezolveAudioVizualizationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +57,8 @@ public class ScanActivity extends AppCompatActivity implements ScanManagerInterf
         scanManager.stopVideoScan();
         scanManager.destroy();
         scanManager.startVideoScan(this, scanView);
+        scanManager.startAudioScan(this, rezolveAudioVizualizationView);
+
     }
 
     @Override
