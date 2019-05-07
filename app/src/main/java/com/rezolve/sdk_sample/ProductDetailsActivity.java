a5174928849beff30fd9585670b8648be3120d8c
Artur Teodorowicz <artur.teodorowicz@infullmobile.com>
2019-05-07 09:41:28 +0200

    Small code cleanup

--- app/src/main/java/com/rezolve/sdk_sample/ProductDetailsActivity.java
+++ app/src/main/java/com/rezolve/sdk_sample/ProductDetailsActivity.java
@@ -29,7 +29,7 @@ import java.util.List;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
-    SpinKitView loadingSpinView;
+    private SpinKitView loadingSpinView;
     private CarouselView previewCarouselView;
     private TextView titleTextView;
     private TextView priceTextView;
