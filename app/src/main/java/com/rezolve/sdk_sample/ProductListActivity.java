c5d8b4882b5565ed3d1557cd7111e8141c3d0f99
Zbigniew Krawczyk <zbigniew.krawczyk@likims.com>
2019-08-23 14:47:41 +0200

    [RZ-5358] Added workaround for merchant list and added banner and dummy loading to the Product List Screen

--- app/src/main/java/com/rezolve/sdk_sample/ProductListActivity.java
+++ app/src/main/java/com/rezolve/sdk_sample/ProductListActivity.java
@@ -1,13 +1,79 @@
 package com.rezolve.sdk_sample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.github.ybq.android.spinkit.SpinKitView;
+import com.rezolve.sdk.RezolveSDK;
 
 public class ProductListActivity extends AppCompatActivity {
+
+    public static final String PARAM_MERCHANT_ID_KEY = "merchantId";
+    public static final String PARAM_MERCHANT_BANNER_KEY = "merchantBanner";
+    private SpinKitView loadingSpinView; // TODO: code duplication
+    private RezolveSDK rezolveSdk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+
+        // Get external parameters
+        Intent intent = getIntent();
+        String merchantId = null;
+        String merchantBanner = null;
+        if (intent != null) {
+            merchantId = intent.getStringExtra(PARAM_MERCHANT_ID_KEY);
+            merchantBanner = intent.getStringExtra(PARAM_MERCHANT_BANNER_KEY);
+        }
+
+        // Bind views
+        loadingSpinView = findViewById(R.id.loadingSpinView); // TODO: code duplication
+        ImageView merchantBannerView = findViewById(R.id.ivMerchantBanner);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        // global
+        rezolveSdk = RezolveSDK.peekInstance();
+
+        // load data
+        loadBanner(merchantBannerView, merchantBanner);
+        loadProducts(recyclerView, merchantId);
     }
+
+    // TODO: code duplication:
+    private void displayLoadingIndicator() {
+        loadingSpinView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideLoadingIndicator() {
+        loadingSpinView.setVisibility(View.INVISIBLE);
+    }
+    // TODO: code duplication ^^^
+
+    private void loadBanner(ImageView merchantBannerView, String merchantBanner) {
+        Glide.with(this)
+                .load(merchantBanner)
+                .placeholder(R.drawable.ic_slider_head)
+                .error(android.R.drawable.stat_notify_error)
+                .dontTransform()
+                .into(merchantBannerView);
+
+    }
+
+    private void loadProducts(RecyclerView recyclerView, String merchantId) {
+        // FIXME: implement this method!
+        displayLoadingIndicator();
+        new Handler().postDelayed(() -> {
+            Toast.makeText(this, merchantId, Toast.LENGTH_LONG).show();
+            hideLoadingIndicator();
+        }, 3000);
+    }
+
 }
--- app/src/main/java/com/rezolve/sdk_sample/ScanActivity.java
+++ app/src/main/java/com/rezolve/sdk_sample/ScanActivity.java
@@ -2,26 +2,38 @@ package com.rezolve.sdk_sample;
 
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.github.ybq.android.spinkit.SpinKitView;
 import com.nabinbhandari.android.permissions.PermissionHandler;
 import com.nabinbhandari.android.permissions.Permissions;
 import com.rezolve.sdk.RezolveSDK;
+import com.rezolve.sdk.core.interfaces.MerchantInterface;
 import com.rezolve.sdk.core.interfaces.ScanManagerInterface;
+import com.rezolve.sdk.core.managers.MerchantManager;
 import com.rezolve.sdk.core.managers.ScanManager;
 import com.rezolve.sdk.model.foreign.RezolveScanResult;
 import com.rezolve.sdk.model.network.RezolveError;
 import com.rezolve.sdk.model.shop.Category;
+import com.rezolve.sdk.model.shop.Merchant;
 import com.rezolve.sdk.model.shop.Product;
 import com.rezolve.sdk.model.shop.ScannedData;
+import com.rezolve.sdk.model.shop.SupportedDeliveryMethod;
 import com.rezolve.sdk.views.RezolveScanView;
+
+import java.util.List;
 
 public class ScanActivity extends AppCompatActivity implements ScanManagerInterface {
 
+    private static final String TAG = ScanActivity.class.getSimpleName();
     private RezolveSDK rezolveSdk;
 
     private SpinKitView loadingSpinView;
@@ -36,15 +48,6 @@ public class ScanActivity extends AppCompatActivity implements ScanManagerInterf
 
         initFab();
 
-    }
-
-    private void initFab() {
-        fabMain = findViewById(R.id.fabMain);
-        if (fabMain != null) {
-            fabMain.setOnClickListener(view -> {
-                navigateToProductListView();
-            });
-        }
     }
 
     @Override
@@ -120,8 +123,78 @@ public class ScanActivity extends AppCompatActivity implements ScanManagerInterf
         startActivity(intent);
     }
 
-    private void navigateToProductListView() {
+    //#
+    //# MENU ACTION AND MERCHANT LIST WORKAROUND:
+    //#
+
+    private void initFab() {
+        fabMain = findViewById(R.id.fabMain);
+        if (fabMain != null) {
+            fabMain.setOnClickListener(this::onFabMainClick);
+        }
+    }
+
+    private void onFabMainClick(View view) {
+        displayLoadingIndicator();
+        requestMerchantList(MerchantManager.MerchantVisibility.ALL, new MerchantInterface() {
+            @Override
+            public void onGetMerchantsSuccess(List<Merchant> merchantList) {
+                hideLoadingIndicator();
+                if (merchantList != null && merchantList.size() > 0) {
+                    navigateToProductListView(merchantList.get(0));
+                } else {
+                    showSnackbar(view, getString(R.string.missing_merchants), Color.YELLOW);
+                }
+            }
+
+            @Override
+            public void onGetShippingMethodsSuccess(List<SupportedDeliveryMethod> list) {
+                hideLoadingIndicator();
+            }
+
+            @Override
+            public void onError(@NonNull RezolveError rezolveError) {
+                hideLoadingIndicator();
+                String errorMessage = String.format(
+                        getString(R.string.rezolve_error_format),
+                        rezolveError.getErrorType().toString(),
+                        rezolveError.getErrorMessage().toString(),
+                        rezolveError.getMessage() == null ? "NULL" : rezolveError.getMessage()
+                );
+                Log.e(TAG, errorMessage);
+                showSnackbar(view, errorMessage, Color.RED);
+            }
+        });
+    }
+
+    private void requestMerchantList(MerchantManager.MerchantVisibility merchantVisibility, MerchantInterface merchantInterface) {
+        MerchantManager merchantManager = rezolveSdk.getRezolveSession() == null ? null : rezolveSdk.getRezolveSession().getMerchantManager();
+        if (merchantManager == null) {
+            merchantInterface.onError(new RezolveError(RezolveError.RezolveErrorType.CUSTOM, RezolveError.RezolveErrorMessage.CUSTOM, "Missing Merchant Manager"));
+        } else {
+            merchantManager.getMerchants(getApplicationContext(), merchantVisibility, merchantInterface);
+        }
+    }
+
+    private void showSnackbar(View view, String message, @ColorInt int color) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT);
+        snackbar.getView().setBackgroundColor(color);
+        snackbar.show();
+    }
+
+    private void navigateToProductListView(Merchant merchant) {
         Intent intent = new Intent(ScanActivity.this, ProductListActivity.class);
+        Bundle bundle = new Bundle();
+
+        bundle.putString(ProductListActivity.PARAM_MERCHANT_ID_KEY, merchant.getId());
+        bundle.putString(ProductListActivity.PARAM_MERCHANT_BANNER_KEY, merchant.getBanner());
+        intent.putExtras(bundle);
+
         startActivity(intent);
     }
+
+    //#
+    //# MENU ACTION AND MERCHANT LIST WORKAROUND ^^^
+    //#
+
 }
